d5c633a9cc1f487f017b6cb5002bd57d5d2d64b4
Apprentice <nealcpeters@gmail.com>
2014-02-21 16:47:01 -0600

    game logic works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,30 +12,28 @@ get '/:deck_name/end' do
   "You feeenished"
 end
 
-get '/:deck_name/:card_number' do
+get "/:deck_name/:card_number" do
   @deck = Deck.find_by_name(params[:deck_name]).cards
-
-  if params[:card_number].to_i >= @deck.length
-    redirect "/#{params[:deck_name]}/end"
-  end
-
   erb :game_play
 end
 
 
 #POST===========================================
 
-
-post '/start/' do
-  @name = params[:name]
-  redirect '/start/#{@name}'
-
-
-end
-
 post '/:deck_name/:card_number' do
-  # @card = Card.find_by_name()
-  num = params[:card_number].to_i
-  num += 1
-  redirect "/#{params[:deck_name]}/#{num}"
+  @deck = Deck.find_by_name(params[:deck_name]).cards
+  @num = params[:card_number].to_i
+  card = Card.find_by_question(params[:question])
+
+  if params[:answer] == card.answer
+    @deck_name = params[:deck_name]
+    @num += 1
+    if @num >= @deck.length
+      redirect "/#{params[:deck_name]}/end"
+    end
+    erb :correct_answer
+  else
+    @correct = false
+    erb :game_play
+  end
 end
