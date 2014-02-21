54aca88e311ad9966db3240a944ec553dbb81d93
Apprentice <nealcpeters@gmail.com>
2014-02-21 13:49:51 -0600

    basic structure

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,5 +10,18 @@ end
 
 
 get '/:deck_name' do
+  @deck = Deck.find_by_name(params[:deck_name]).cards
+  @deck_name = @deck.name
+  erb :test_game
+end
+
+get '/:deck_name/end' do
+
+end
+#POST===========================================
+
+
+post '/:deck_name' do
+
 
 end
